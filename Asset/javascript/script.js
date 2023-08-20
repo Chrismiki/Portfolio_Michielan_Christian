f37086c6d99e9c9b1a7f85c0c653cd03194a261b
Chrismiki <miki.chri@hotmail.com>
2023-08-20 11:23:28 +0200

    ho reso il modulo funzionante graze a emailjs

--- Asset/javascript/script.js
+++ Asset/javascript/script.js
@@ -13,4 +13,25 @@ document.querySelectorAll(".nav-link").forEach (n => n.addEventListener("click",
 
 
 
+    
 
+
+    
+    (function() {
+        // https://dashboard.emailjs.com/admin/account
+        emailjs.init('7UzpYsMgQ678ID88a');
+    })();
+
+
+    window.onload = function() {
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            // these IDs from the previous steps
+            emailjs.sendForm('contact_service', 'contact_form', this)
+                .then(function() {
+                    console.log('SUCCESS!');
+                }, function(error) {
+                    console.log('FAILED...', error);
+                });
+        });
+    }
