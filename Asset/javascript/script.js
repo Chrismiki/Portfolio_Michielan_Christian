bd71bbb8dde317d22ac878f3c5595e3b5ac6c750
Chrismiki <miki.chri@hotmail.com>
2023-08-20 13:26:55 +0200

    agiustato elementi scritti male

--- Asset/javascript/script.js
+++ Asset/javascript/script.js
@@ -20,7 +20,7 @@ function SendMail() {
     var params = {
         from_name : document.getElementById("fullname").value,
         email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value
+        message_id : document.getElementById("message").value
     }
     emailjs.send("service_qfn82if", "template_4vw2atg", params).then(function(res){
         alert("Success! " + res.status);
